2f78f5df74d8aad8656ead8061b6b0716e3c4cf1
azizlsd <mouhamedaziz.boulifi@esprit.tn>
2025-07-23 00:32:16 +0200

    restruct

---
+++ works.js
@@ -0,0 +1,15 @@
+// works.js
+// Array of all works (projects and cover art)
+const works = [
+  {
+    image: 'images/CARPEDIEM1.png',
+    title: 'Carpe Diem',
+    link: 'coverart.html#carpe-diem'
+  },
+  // Add more works here, e.g.:
+  // {
+  //   image: 'images/PROJECT2.png',
+  //   title: 'Project 2',
+  //   link: 'projects.html#project-2'
+  // },
+]; 
